30cd894fb62336b5e04fd31e72220ceda2afcf14
herringg369 <herringg369@gmail.com>
2021-10-29 10:41:10 -0400

    have the first equation for Canadian currency working

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ let can = document.getElementById('can1')
 can.innerText = 'Canadian Dollars'
 can.addEventListener('click', function() {
 let canCurrency = prompt('How much do you have in Canadian Dollars?', 0)
+// console.log(`Canadian Currency Value: ${canCurrency}`)
+let canWrite = document.getElementById('canDiv')
 
-function cdToUsd() {
-
-    if (canCurrency = !isNaN) {
-    let convertCdToUsd = (canCurrency * .81)
-    console.log(convertCdToUsd)
-    } else {
-        console.log('Please insert a numerical value')
+    if (canCurrency >= 0.00 && canCurrency <= 1000000000000000000000000000) {
+    let convertCdToUsd = (canCurrency * .81).toFixed(2)
+    console.log(`Canadian Currency Value: ${canCurrency}`)
+    console.log(`US Currency Value: ${convertCdToUsd}`)
     }
-}
+    else {
+    console.log('Please insert a valid currency amount')   
+    }
+})
+
 
 /*
 document.querySelector('#searchText').addEventListener('input', function (e) {
