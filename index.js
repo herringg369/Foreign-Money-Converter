4aafb7903a6105917479253f2e34ff723e9406c4
herringg369 <herringg369@gmail.com>
2021-12-03 09:39:58 -0500

    added Pesos, and Euros

--- index.js
+++ index.js
@@ -15,6 +15,37 @@ let canWrite = document.getElementById('canDiv')
     }
 })
 
+let mex = document.getElementById('mex1')
+mex.innerText = 'Mexican Pesos'
+mex.addEventListener('click', function() {
+let mexCurrency = prompt('How much do you have in Mexican Pesos?', 0)
+// console.log(`Mexican Currency Value: ${mexCurrency}`)
+
+    if (mexCurrency >= 0.00 && mexCurrency <= 1000000000000000000000000000) {
+    let convertMpToUsd = (mexCurrency * .047).toFixed(2)
+    console.log(`Mexican Currency Value: ${mexCurrency}`)
+    console.log(`US Currency Value: ${convertMpToUsd}`)
+    }
+    else {
+    console.log('Please insert a valid currency amount')   
+    }
+})
+
+let europe = document.getElementById('europe')
+europe.innerText = 'Euros'
+europe.addEventListener('click', function() {
+let euroCurrency = prompt('How much do you have in Euros?', 0)
+// console.log(`Mexican Currency Value: ${mexCurrency}`)
+
+    if (euroCurrency >= 0.00 && euroCurrency <= 1000000000000000000000000000) {
+    let convertEToUsd = (euroCurrency * 1.13).toFixed(2)
+    console.log(`European Currency Value: ${euroCurrency}`)
+    console.log(`US Currency Value: ${convertEToUsd}`)
+    }
+    else {
+    console.log('Please insert a valid currency amount')   
+    }
+})
 
 /*
 document.querySelector('#searchText').addEventListener('input', function (e) {
